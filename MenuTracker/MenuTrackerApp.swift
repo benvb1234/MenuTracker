9f2cc27b23ab92f5cd53e3ddeafc6ec47b201c8a
benvb1234 <benvb1234@gmail.com>
2024-02-02 18:56:09 -0600

    Initial Commit

---
+++ MenuTracker/MenuTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTrackerApp.swift
+//  MenuTracker
+//
+//  Created by Ben Valdebenito on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
